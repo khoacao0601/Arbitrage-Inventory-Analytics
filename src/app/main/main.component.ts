bd4237025d117afc2e563e7921762932720e283c
Khoa Cao <khoacao0601@gmail.com>
2026-06-06 19:42:32 -0500

    update table

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SideBarComponent } from '../side-bar/side-bar.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, SideBarComponent],
+  imports: [HeaderComponent, SideBarComponent, TableComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
